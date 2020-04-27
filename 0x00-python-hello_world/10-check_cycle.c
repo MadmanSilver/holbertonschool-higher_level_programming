7f61ddb45208adff807f150baa766aea5a8a143a
Sean Taylor <silverwolf4350@gmail.com>
2020-04-27 16:18:22 +0000

    Created all files for mandatory tasks.

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks a linked list for a cycle
+ * @list: list to check
+ *
+ * Return: 0 if no cycle, 1 otherwise
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *p1 = list, *p2 = list->next;
+
+	while (p1 != NULL && p2 != NULL)
+	{
+		if (p1 == p2)
+			return (1);
+		p1 = p1->next;
+		p2 = p2->next;
+		if (p2 != NULL)
+			p2 = p2->next;
+	}
+
+	return (0);
+}
---
+++ 0x00-python-hello_world/README.md
@@ -0,0 +1,1 @@
+0x00 Python - Hello, World is a Holberton project intended as an introduction to the Python programming language.
