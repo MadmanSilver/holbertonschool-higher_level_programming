7d4d1c60e6397bd58c03cd4d302c07bcd7023c58
Sean Taylor <silverwolf4350@gmail.com>
2020-05-19 22:13:20 +0000

    Added tuple len check and fixed checks inside setter.

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -17,7 +17,7 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-        if type(position) is not tuple:
+        if type(position) is not tuple or len(position) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
         if type(position[0]) is not int or type(position[1]) is not int\
                 or position[0] < 0 or position[1] < 0:
@@ -44,12 +44,12 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if type(position) is not tuple:
+        if type(value) is not tuple or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
-        if type(position[0]) is not int or type(position[1]) is not int\
-                or position[0] < 0 or position[1] < 0:
+        if type(value[0]) is not int or type(value[1]) is not int\
+                or value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = position
+        self.__position = value
 
     def area(self):
         """Calculates the area of the square
