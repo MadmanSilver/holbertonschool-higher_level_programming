537686742ca776154cedb39fad81a4d020588009
Sean Taylor <silverwolf4350@gmail.com>
2020-05-20 01:54:31 +0000

    Fixed line count.

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
+"""Contains MagicClass class."""
 import math
+
 
 class MagicClass:
     """A circle!"""
     def __init__(self, radius):
-        """Initializes radius
-        Args:
-            radius (int, float): radius of circle"""
+        """Initializes radius"""
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
