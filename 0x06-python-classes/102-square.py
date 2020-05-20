dd40b7a7e5e306f96f49b6b853f63ee9b0b352a3
Sean Taylor <silverwolf4350@gmail.com>
2020-05-20 01:38:15 +0000

    Switched to entirely explicit.

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 """Contains the Square class."""
-import functools
 
 
-@functools.total_ordering
 class Square:
     """A square."""
     def __init__(self, size=0):
@@ -42,7 +40,25 @@ class Square:
         return self.__size ** 2
 
     def __lt__(self, other):
+        """less than comparison"""
         return self.area() < other.area()
 
     def __eq__(self, other):
+        """equal to comparison"""
+        return self.area() == other.area()
+
+    def __le__(self, other):
+        """less than or equal to comparison"""
+        return self.area() < other.area()
+
+    def __ne__(self, other):
+        """not equal to comparison"""
+        return self.area() == other.area()
+
+    def __gt__(self, other):
+        """greater than comparison"""
+        return self.area() < other.area()
+
+    def __ge__(self, other):
+        """greater than or equal to comparison"""
         return self.area() == other.area()
