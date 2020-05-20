3f30e4c31a6f8b23ffff9f45dc797d8b10d54bd3
Sean Taylor <silverwolf4350@gmail.com>
2020-05-20 01:59:40 +0000

    Added default for radius

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -5,7 +5,7 @@ import math
 
 class MagicClass:
     """A circle!"""
-    def __init__(self, radius):
+    def __init__(self, radius=0):
         """Initializes radius"""
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
