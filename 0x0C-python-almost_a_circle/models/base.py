a92306f3a88ae201eb6ab4beea60009196003080
Sean Taylor <silverwolf4350@gmail.com>
2020-06-09 17:53:34 +0000

    Modified conditional with direct value check.

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -17,7 +17,7 @@ class Base():
 
     def to_json_string(list_dictionaries):
         """ Returns the JSON string representation of list_dictionaries. """
-        if list_dictionaries is None or list_dictionaries is []:
+        if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
 
