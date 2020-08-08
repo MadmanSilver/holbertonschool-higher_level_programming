5dd2f1fe2abf0faaf535b0fdb2a965eb39c03f05
Sean Taylor <silverwolf4350@gmail.com>
2020-08-08 22:06:50 +0000

    Removed module alias.

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """ lists all states that start with N from hbtn_0e_0_usa """
-import MySQLdb as mysql
+import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    conn = mysql.connect(host="localhost", port=3306, user=sys.argv[1],
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = conn.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
