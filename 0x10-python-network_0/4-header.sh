aa980ecef6b0cc9501dbc2b9c075e617c75a5d26
Sean Taylor <silverwolf4350@gmail.com>
2020-08-18 22:48:50 +0000

    Added -s to all

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # send header variable and display
-curl -H "X-HolbertonSchool-User-Id:98" "$1"
+curl -sH "X-HolbertonSchool-User-Id:98" "$1"
--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # send post variables and display
-curl -d "email=hr@holbertonschool.com" -d "subject=I will always be here for PLD" "$1"
+curl -sd "email=hr@holbertonschool.com" -d "subject=I will always be here for PLD" "$1"
