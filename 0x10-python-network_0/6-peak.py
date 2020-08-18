f0b4cb1fdfccfdc1b0eb379686d488352923c7b2
Sean Taylor <silverwolf4350@gmail.com>
2020-08-18 23:22:47 +0000

    Switched to divide and conquer strategy.

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,13 +3,21 @@
 
 
 def find_peak(list_of_integers):
-    """ finds a peak """
-    num = list_of_integers
-    res = None
-    for i in range(len(num)):
-        res = num[i]
-        if num[i - 1] < num[i] and num[i + 1] < num[i]:
-            return num[i]
-        if num[i] != num[i - 1]:
-            res = None
-    return res
+    """ calls recursive peak """
+    if not len(list_of_integers):
+        return None
+    return list_of_integers[peak(list_of_integers, 0,
+                                 len(list_of_integers) - 1,
+                                 len(list_of_integers))]
+
+
+def peak(ar, lo, hi, ln):
+    """ finds peak """
+    mid = int(lo + (hi - lo) / 2)
+    if (mid == 0 or ar[mid - 1] <= ar[mid]) and\
+       (mid == ln - 1 or ar[mid + 1] <= ar[mid]):
+        return mid
+    elif mid > 0 and ar[mid - 1] > ar[mid]:
+        return peak(ar, lo, mid - 1, ln)
+    else:
+        return peak(ar, mid + 1, hi, ln)
